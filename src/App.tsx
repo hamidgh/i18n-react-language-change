9ae7d41620527695d739368671115b0bd09a3ed9
Hamid Ghoraishi <hamid.ghoraishi2@wnco.com>
2022-06-05 19:05:37 -0500

    feat: Create a form with react bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
-import {Button} from "react-bootstrap";
+import {Button, Form} from "react-bootstrap";
 
 function App() {
+  const [inputField, setInputField] = useState({text: ''});
+
+  const handleOnChangeInput = (e: any) => {
+    setInputField({text: e.target.value})
+    console.log(inputField.text);
+  }
+
+  const handleOnClick = () => {
+    if(inputField.text) {
+      console.log(inputField.text);
+      setInputField({text: ''});
+    }
+  }
+
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>To do Item</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your todo Item"
+            onChange={handleOnChangeInput}
+            value={inputField.text}
+          />
+        </Form.Group>
+        <Button
+          variant="primary"
+          onClick={handleOnClick}
+        > Submit</Button>
+      </Form>
     </div>
   );
 }
