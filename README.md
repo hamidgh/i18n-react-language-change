304b5f528f30965a1cc64015a74d6fd45841bb5b
Hamid Ghoraishi <hamid.ghoraishi2@wnco.com>
2022-06-25 23:30:16 -0500

    created responsive design app

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# UI Layout with Flexbox
+# Responsive Design - Media Query
 
-You can watch step by step of this tutorial from this link [YouTube Video](https://youtu.be/hIGF5O0L0E0).
+You can watch step by step of this tutorial from this link [YouTube Video](hhttps://youtu.be/u6bhgo536-w).
 
 ## useful links that were used in tutorial
-1- [Flex documentation](https://css-tricks.com/snippets/css/a-guide-to-flexbox/)
+1- [Media Query documentation](https://developer.mozilla.org/en-US/docs/Web/CSS/Media_Queries/Using_media_queries)
 
 ## How to run the app
 1- In the project directory, run: `npm install`\
--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -5,12 +5,16 @@ const HomePage = () => {
   return (
     <div>
       <div className="container">
-        <div className="item box1"> 1 </div>
-        <div className="item box2"> 2 </div>
-        <div className="item box3"> 3 </div>
-        <div className="item box4"> 4 </div>
-        <div className="item box5"> 5 </div>
-        <div className="item box6"> 6 </div>
+        <div className="item"> 1 </div>
+        <div className="item"> 2 </div>
+        <div className="item"> 3 </div>
+        <div className="item"> 4 </div>
+        <div className="item"> 5 </div>
+        <div className="item"> 6 </div>
+      </div>
+      <div className="container">
+        <div className="pic1">Picture 1</div>
+        <div className="pic2">Picture 2</div>
       </div>
     </div>
   );
